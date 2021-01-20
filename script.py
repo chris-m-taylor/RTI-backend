631d586bbe084137c4304dbbfb2e17919b03ef0a
Chris Taylor <chrismtaylor1998@gmail.com>
2021-01-20 18:21:46 -0500

    added feature, workers over 50k

--- script.py
+++ script.py
@@ -20,6 +20,15 @@ totalWomen = 0
 men50k = 0
 women50k = 0
 
+#Government vs Private pay vs Self Employed
+totalGov = 0
+totalPrivate = 0
+totalSelfEm = 0
+
+gov50k = 0
+private50k = 0
+selfEm50k = 0
+
 
 with open('flat-csvV2.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -43,11 +52,26 @@ with open('flat-csvV2.csv', newline='') as csvfile:
             if (row['over_50k'] == '1'):
                 men50k += 1
         
-        if (row['sex'] == 'Female'):
+        elif (row['sex'] == 'Female'):
             totalWomen += 1
             if (row['over_50k'] == '1'):
                 women50k += 1
+
+        # government vs private pay vs self employed
+        if (row['workclass'][-3:] == 'gov'):
+            totalGov += 1
+            if (row['over_50k'] == '1'):
+                gov50k += 1
         
+        elif (row['workclass'] == 'Private'):
+            totalPrivate += 1
+            if (row['over_50k'] == '1'):
+                private50k += 1
+        else:
+            totalSelfEm += 1
+            if (row['over_50k'] == '1'):
+                selfEm50k += 1
+
         
 print("Only {:d}% of high school graduates make over 50k while {:d}% of college graduates with a bachelors degree make over 50k".format(
     int(hsGrad50k/totalHsGrad * 100), int(bach50k/totalBach * 100)
@@ -57,3 +81,6 @@ print("Only {:d}% of women make over 50k while {:d}% of men make over 50k".forma
     int(women50k/totalWomen * 100), int(men50k/totalMen * 100)
 ))
 
+print("{:d}% of government workers make over 50k, {:d}% of private sector employees make over 50k, and {:d}% of self employed individuals make over 50k".format(
+    int(gov50k/totalGov * 100), int(private50k/totalPrivate * 100), int(selfEm50k/totalSelfEm * 100)
+))
